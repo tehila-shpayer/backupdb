fc19f63e4b8ef1bba73f42f145cd947afff007c1
tehila1742 <tehila1742@gmail.com>
2022-10-27 13:33:33 +0300

    d

--- script.sh
+++ script.sh
@@ -99,7 +99,7 @@ get_credential() {
 	# 	((i = i + 1))
 	# done < tmp
 	# rm tmp
-	mongodump mongodb+srv://$HOST:$HOST --authenticationDatabase admin --username=$USER --password=$PASSWORD --db amigo-hospital --out ./backups/mongo
+	mongodump mongodb+srv://$HOST --port:$PORT --authenticationDatabase admin --username=$USER --password=$PASSWORD --db amigo-hospital --out ./backups/mongo
 }
 conduct_mysql_backup() {
 	USER=$1
