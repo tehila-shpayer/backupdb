bc2991596ea35e7b2c08b6ef7aa745ea45e7fa5a
tehila1742 <tehila1742@gmail.com>
2022-10-27 12:43:41 +0300

    mongo connection

--- mongo.js
+++ mongo.js
@@ -1,6 +1,3 @@
-// conn = new Mongo();
-// dbadmin = conn.getDB("admin");
-// printjson(dbadmin.adminCommand("listDatabases")).forEach((db) => {
-//   printjson(db.getName());
-// });
-printjson(db.adminCommand("listDatabases"));
+printjson(db.adminCommand("listDatabases")["databases"]).forEach((db) => {
+  printjson(db.getName());
+});
